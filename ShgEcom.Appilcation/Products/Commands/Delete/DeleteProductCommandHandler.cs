ac9fde58f03e4b801c15bddc446b30c9c964dafd
jkjoseph2023@gmail.com <jkjoseph2023@gmail.com>
2025-02-11 09:19:24 +0530

    Cache Delete for Update and Delete

--- ShgEcom.Appilcation/Products/Commands/Delete/DeleteProductCommandHandler.cs
+++ ShgEcom.Appilcation/Products/Commands/Delete/DeleteProductCommandHandler.cs
@@ -1,15 +1,18 @@
 ﻿using ErrorOr;
 using MediatR;
+using Microsoft.Extensions.Caching.Memory;
+using ShgEcom.Application.Common.Constants;
 using ShgEcom.Application.Common.Interfaces.Persistence;
 
 namespace ShgEcom.Application.Products.Commands.Delete
 {
-    public class DeleteProductCommandHandler(IProductRepository productRepository) : IRequestHandler<DeleteProductCommand, ErrorOr<bool>>
+    public class DeleteProductCommandHandler(IProductRepository productRepository, IMemoryCache memoryCache) : IRequestHandler<DeleteProductCommand, ErrorOr<bool>>
     {
         private readonly IProductRepository _productRepository = productRepository;
 
         public async Task<ErrorOr<bool>> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
         {
+            memoryCache.Remove(CacheConstants.ProductsListKey);
             var product = await _productRepository.GetByIdAsync(request.Id);
 
             if (product is null)
--- ShgEcom.Appilcation/Products/Commands/Update/UpdateProductCommandHandler.cs
+++ ShgEcom.Appilcation/Products/Commands/Update/UpdateProductCommandHandler.cs
@@ -1,17 +1,19 @@
 ﻿using ErrorOr;
 using MediatR;
+using Microsoft.Extensions.Caching.Memory;
+using ShgEcom.Application.Common.Constants;
 using ShgEcom.Application.Common.Interfaces.Persistence;
 using ShgEcom.Application.Common.Interfaces.Services;
 using ShgEcom.Application.Products.Common;
 
 namespace ShgEcom.Application.Products.Commands.Update
 {
-    public class UpdateProductCommandHandler(IProductRepository productRepository, IDateTimeProvider dateTimeProvider) : IRequestHandler<UpdateProductCommand, ErrorOr<ProductResult>>
+    public class UpdateProductCommandHandler(IProductRepository _productRepository, IDateTimeProvider dateTimeProvider, IMemoryCache memoryCache) : IRequestHandler<UpdateProductCommand, ErrorOr<ProductResult>>
     {
-        private readonly IProductRepository _productRepository = productRepository;
 
         public async Task<ErrorOr<ProductResult>> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
         {
+            memoryCache.Remove(CacheConstants.ProductsListKey);
             var product = await _productRepository.GetByIdAsync(request.Id);
 
             if (product is null)
